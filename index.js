b69693c36197fd181f12c165e387983c9b708cc0
Daniel James <djames@syncromatics.com>
2018-01-15 16:49:03 -0800

    Added TODOs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const webshot = require('webshot');
 const options = {
     windowSize: {
+        // Twitter card size
         width: 1024,
         height: 512,
     },
@@ -9,7 +10,9 @@ const options = {
             document.getElementById('text').innerText = this.text;
         },
         context: {
+            // TODO: figure out text to pass to template
             text: 'WINTER IS COMING. SEEK IMMEDIATE SHELTER. THIS IS NOT A DRILL.',
+            // TODO: Add a random 'N mins ago'
         }
     }
 };
@@ -18,5 +21,6 @@ const iosEmergencyAlertsTemplate = ['file://']
     .concat(__dirname.split(/[\\/]+/))
     .concat(['iosEmergencyAlerts', 'template.html'])
     .join('/');
-console.log('Requesting URL', iosEmergencyAlertsTemplate);
+
+// TODO: change logic to not use temp file if possible, then tweet resulting image
 webshot(iosEmergencyAlertsTemplate, 'test.png', options, console.error);
